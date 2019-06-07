b10b4a4031d0f7bb5767d51db68f4af92abe720f
Denny Biasiolli <denny.biasiolli@gmail.com>
2019-06-07 20:40:00 +0200

    src(parser): parsing product line

--- src/file_parser.py
+++ src/file_parser.py
@@ -33,9 +33,25 @@ class FileParser:
             return
 
         # standard line
+        self.parse_product_line(line)
         self.lines_in_section += 1
         print(line)
 
     def print_summary(self):
         print(f'Sales Taxes: {self.taxes}')
         print(f'Total: {self.total}')
+
+    def parse_product_line(self, line):
+        input_match = re.search(
+            '^([\\d]+) ([a-zA-Z ]+) at ([\\d]+.[\\d]+)$',
+            line
+        )
+        if input_match is None or len(input_match.groups()) != 3:
+            raise Exception(f'Line not recognized: "{line}"')
+
+        [str_quantity, description, str_price] = input_match.groups()
+        quantity = int(str_quantity)
+        price = float(str_price)
+        is_imported = False
+        if 'imported ' in description:
+            is_imported = True
