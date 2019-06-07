85baefb1ed62283d9ecb18b35879b603564d2101
Denny Biasiolli <denny.biasiolli@gmail.com>
2019-06-07 17:42:44 +0200

    src: adding an empty parser class

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,7 @@
+from .file_parser import FileParser
+
 def parse_files(input_paths):
-    pass
+    for path in input_paths:
+        p = FileParser(path)
+        p.parse()
+        print()
---
+++ src/file_parser.py
@@ -0,0 +1,9 @@
+import re
+
+
+class FileParser:
+    def __init__(self, path):
+        self.path = path
+
+    def parse(self):
+        pass
